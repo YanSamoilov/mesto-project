96c0d7174380c71b8a32e403eb15c256ae6a3037
Yan Samoilov <janchik31@yandex.ru>
2021-09-02 16:54:48 +0300

    Рефакторинг функций присваивания слушателей карточки и добавление фуекции сброса валидации при повторном открытии

--- src/components/card.js
+++ src/components/card.js
@@ -1,5 +1,5 @@
 export {createCard, submitFormNewCard, handleDeleteCard, largeImagePopup};
-import {openPopup, handleViewImage, closePopup, cardAddPopup} from './modal.js';
+import {openPopup, handleOpenImagePopup, closePopup, cardAddPopup} from './modal.js';
 import {cardsList, addCardForm} from '../pages/index.js';
 
 const largeImagePopup = document.querySelector('#popup-view-image');  // Попап просмотра увеличенного изображения.
@@ -13,23 +13,23 @@ const createCard = (item) => {
   cardElement.querySelector('.cards__title').textContent = item.name;
   cardElement.querySelector('.cards__image').src = item.link;
   cardElement.querySelector('.cards__image').alt = item.alt;
-  addCardListeners(cardElement);
+  cardElement.querySelector('.cards__like').addEventListener('click', toogleLikeCard);
+  cardElement.querySelector('.cards__button-bin').addEventListener('click', handleDeleteCard);
+  cardElement.querySelector('.cards__image').addEventListener('click', () =>{
+    handleOpenImagePopup(item.name, item.link, item.alt);
+  });
   return cardElement;
 }
 
-// Слушатели увеличения картинки, лайка, удаления на каждую карточку
-const addCardListeners = (cardElement) => {
-  cardElement.addEventListener('click', (evt) => {
-    const evtClassList = evt.target.classList;
-    if (evtClassList.contains('cards__like')) {
-      evtClassList.toggle('cards__like_active');
-    } else if (evtClassList.contains('cards__button-bin')) {
-      handleDeleteCard(evt.target);
-    } else if (evtClassList.contains('cards__image')) {
-      openPopup(largeImagePopup);
-      handleViewImage(evt.target);
-    }
-  })
+//Тоггл лайка на карточке
+const toogleLikeCard = (evt) => {
+  evt.target.classList.toggle('cards__like_active');
+}
+
+//Удалить карточку
+const handleDeleteCard = (evt) => {
+  const card = evt.target.closest('.cards__list-elem');
+  card.remove();
 }
 
 // Добавление новой карточки пользователем в начало списка
@@ -43,10 +43,4 @@ const submitFormNewCard = (event) => {
   cardsList.prepend(createCard(card));
   closePopup(cardAddPopup);
   addCardForm.reset();
-}
-
-// Удаление карточки места
-const handleDeleteCard = (button) => {
-  const card = button.closest('.cards__list-elem');
-  card.remove();
 }
--- src/components/validation.js
+++ src/components/validation.js
@@ -1,4 +1,4 @@
-export {enableValidationForms};
+export {enableValidationForms, resetValidation};
 
 // Отображать ошибку при заполнении формы
 const showError = (formElem, inputElem, errorMessage, formsData) => {
@@ -31,7 +31,7 @@ const hasInvalidInput = (inputList) => {
   });
 }
 
-// Сменить активность кнопки сохранения popup
+// Сменить активность кнопки сохранения popup при заполнении формы
 const toggleSubmitActivity = (inputList, buttonSubmit, formsData) => {
   if (hasInvalidInput(inputList)) {
     buttonSubmit.classList.add(formsData.inactiveButtonClass);
@@ -61,4 +61,41 @@ const enableValidationForms = (formsData) => {
   formsList.forEach((formElem) => {
     setValidationInput(formElem, formsData);
   })
+}
+
+//Сброс ошибки заполнения формы при повторном открытии
+const resetSpanError = (popup) => {
+  const spanErrorArray = popup.querySelectorAll('.popup__error');
+  spanErrorArray.forEach((spanError) => {
+    if(spanError.classList.contains('popup__error_active')) {
+      spanError.classList.remove('popup__error_active');
+      spanError.textContent = '';
+    }
+  });
+}
+
+//Сброс активности формы при повторном открытии
+const resetInputError = (popup) => {
+  const inputArray = popup.querySelectorAll('.popup__form');
+  inputArray.forEach((inputElem) => {
+    if(inputElem.classList.contains('popup__form_type_error')) {
+      inputElem.classList.remove('popup__form_type_error');
+    }
+  })
+}
+
+//Сброс активности кнопки при повторном открытии
+const resetActiveSubmitButton = (popup) => {
+  const buttonSubmit = popup.querySelector('.popup__button-save');
+  if(!buttonSubmit.classList.contains('popup__button-save_inactive')) {
+    buttonSubmit.classList.add('popup__button-save_inactive');
+    buttonSubmit.setAttribute('disabled', 'disabled');
+  }
+}
+
+//Сброс валидации всего попапа при повторном открытии
+const resetValidation = (popup) => {
+  resetActiveSubmitButton(popup);
+  resetSpanError(popup);
+  resetInputError(popup);
 }
