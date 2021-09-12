1866a19f041f44d52481de6f4dd24e9578c849c3
Yan Samoilov <janchik31@yandex.ru>
2021-09-12 21:56:04 +0300

    feature/api

---
+++ src/components/util.js
@@ -0,0 +1,71 @@
+export {toggleCardsBin, addButtonBinCard, markLikedCard, addLike, deleteLike, setButtonSubmitCondition, hidePreloader, confirmDeleteCardPopup, cardTarget};
+import {putLike, deleteLikeRequest} from './api.js';
+import { openPopup } from './modal.js';
+
+const confirmDeleteCardPopup = document.querySelector('#popup-confirm-delete');
+let cardTarget;
+
+//Отображение кнопки удаления карточки в завимости от того кто ее создал
+const toggleCardsBin = (userId, cardOwnerId, cardElement) => {
+  if(userId === cardOwnerId) {
+    addButtonBinCard(cardElement);
+  }
+  else {
+    inactivateButtonBinCard(cardElement);
+  }
+}
+
+//Скрыть прелоадер
+const hidePreloader = () => {
+  document.querySelector('.preloader').remove();
+}
+
+//Изменить состояние кнопки сохранить
+const setButtonSubmitCondition = (button, active) => {
+  button.disabled = !active;
+  active ? button.textContent = "Сохранить" : button.textContent = "Сохранение...";
+}
+
+//Добавить слушатель удаления на карточку
+const addButtonBinCard = (cardElement) => {
+  cardElement.querySelector('.cards__button-bin').addEventListener('click', (evt) => {
+    cardTarget = evt.target.closest('.cards__list-elem');
+    openPopup(confirmDeleteCardPopup);
+  });
+}
+
+//Удалить кнопку удаления карточки
+const inactivateButtonBinCard = (cardElement) => {
+  cardElement.querySelector('.cards__button-bin').classList.add('cards__button-bin_inactive');
+}
+
+//Закрасить собственный лайк при загрузке карточки
+const markLikedCard = (card, likeElem, userId) => {
+  if(card.likes.some(like => like._id === userId)) {
+    likeElem.classList.add('cards__like_active');
+  }
+}
+
+//Поставить лайк на карточке
+const addLike = (cardId, likeCard, cardLikeCount) => {
+  putLike(cardId)
+      .then((data) => {
+        likeCard.classList.add('cards__like_active');
+        cardLikeCount.textContent = data.likes.length;
+      })
+      .catch((err) => {
+        console.log(err);
+      })
+}
+
+//Удалить свой лайк
+const deleteLike = (cardId, likeCard, cardLikeCount) => {
+  deleteLikeRequest(cardId)
+  .then((data) => {
+    likeCard.classList.remove('cards__like_active');
+    cardLikeCount.textContent = data.likes.length;
+  })
+  .catch((err) => {
+    console.log(err);
+  })
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,11 @@
-export {userEditorForm, addCardForm, cardsList};
-import {createCard, submitFormNewCard, largeImagePopup} from '../components/card.js';
-import {initialCards} from '../components/initial-сards.js';
-import {handleOpenUserEditor, submitFormProfile, handleClosePopup, closePopup, openPopup, cardAddPopup} from '../components/modal.js';
+export {userEditorForm, addCardForm, cardsList, avatar, changeAvatarForm};
+import {submitFormNewCard, largeImagePopup, loadCards, handleDeleteCard} from '../components/card.js';
+import {handleOpenUserEditor, submitFormProfile, handleClosePopup, closePopup, openPopup, cardAddPopup, nameUser, activityUser, changeAvatarPopup,
+        submitNewAvatar} from '../components/modal.js';
 import {enableValidationForms} from '../components/validation.js';
+import {setUserInfo} from '../components/user.js';
+import {getInfoArray} from '../components/api.js';
+import {hidePreloader, cardTarget} from '../components/util.js';
 import './index.css';
 
 const buttonUserEdit = document.querySelector('.profile__user-edit');        // Кнопка открытия редактирования профиля.
@@ -10,9 +13,15 @@ const buttonUserEditorClose = document.querySelector('#user-editor-close');  // 
 const buttonAddCard = document.querySelector('.profile__add-card');          // Кнопка добавления карточки.
 const buttonAddCardClose = document.querySelector('#add-card-close');        // Кнопка закрытия окна добавления изображения.
 const buttonCloseViewImage = document.querySelector('#close-larger-image');  // Кнопка закрытия окна просмотра изображения.
+const buttonChangeAvatar = document.querySelector('.profile__change-avatar');    // Кнопка открытия изменения аватара.
 const userEditorForm = document.querySelector('#popup-user-edit-container'); // Форма редактирования профиля.
 const addCardForm = document.querySelector('#popup-add-card-container');     // Форма добавления новой карточки.
 const cardsList = document.querySelector('.cards__list');                    // Список содержащий карточки.
+const avatar = document.querySelector('.profile__avatar');                   // Аватар
+const changeAvatarForm = document.querySelector('#popup-change-avatar-container'); // Форма смены аватара.
+const buttonCloseChangeAvatar = document.querySelector('#change-avatar-close'); //Кнопка закрытия окна изменения аватара.
+const buttonCloseConfirmDelete = document.querySelector('#confirm-delete-close'); //Кнопка закрытия окна изменения аватара.
+const buttonConfirmDelete = document.querySelector('#button-confirm-delete-card'); //Кнопка подтверждения удаления карточки.
 
 // Popup User
 buttonUserEdit.addEventListener('click', handleOpenUserEditor);
@@ -24,10 +33,26 @@ addCardForm.addEventListener('submit', submitFormNewCard);
 buttonAddCardClose.addEventListener('click', handleClosePopup);
 // Popup увеличенного изображения.
 buttonCloseViewImage.addEventListener('click', () => closePopup(largeImagePopup));
-// Расстановка стартовых карточек.
-initialCards.forEach(item => {
-  cardsList.append(createCard(item));
-})
+// Popup обновления аватара
+buttonChangeAvatar.addEventListener('click', () => openPopup(changeAvatarPopup));
+buttonCloseChangeAvatar.addEventListener('click', handleClosePopup);
+changeAvatarForm.addEventListener('submit', submitNewAvatar);
+// Popup подтверждения удаления карточки
+buttonCloseConfirmDelete.addEventListener('click', handleClosePopup);
+buttonConfirmDelete.addEventListener('click', () => handleDeleteCard(cardTarget));
+
+getInfoArray()
+  .then(([userInfo, cards]) => {
+    const userId = userInfo._id;
+    loadCards(cardsList, cards, userId);
+    setUserInfo(nameUser, activityUser, avatar, userInfo);
+  })
+  .catch((err) => {
+    console.log(err);
+  })
+  .finally(() => {
+    hidePreloader();
+  })
 
 enableValidationForms({
   formSelector: '.popup__main-container',
