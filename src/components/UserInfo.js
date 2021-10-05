29cd1ce43bfabcd1baf62db78ad66a9527f395e9
eilerglen <83466883+eilerglen@users.noreply.github.com>
2021-10-05 19:47:55 +0300

    Правка FormValidator и реализация UserInfo

---
+++ src/components/UserInfo.js
@@ -0,0 +1,26 @@
+export default class User {
+  constructor({profileName, profileDescription, profileAvatar}) {
+    this._name = profileName;
+    this._description = profileDescription;
+    this._avatar = profileAvatar;
+  }
+  getUser(){
+    return {name: this._name.textContent,
+            description: this._description.textContent}
+            }
+
+  getUserAvatar() {
+    return this._avatar.src;
+  }
+
+  setUserInfo({name, description}) {
+    this._name.textContent = name;
+    this._description.textContent = description;
+  }
+
+  setUserAvatar(url) {
+    this._avatar.src = url;
+  }
+}
+
+
