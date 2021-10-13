46d02bff56be4576aded2531c95f44c35a8d8407
eilerglen <83466883+eilerglen@users.noreply.github.com>
2021-10-13 13:33:24 +0300

    first commit

--- src/components/api.js
+++ src/components/api.js
@@ -17,7 +17,7 @@ class Api {
   }
 
   //Получение информации о пользователе с сервера
-  _getUserInfo() {
+  getUser() {
     return fetch(`${this.serverURL}users/me`, {
       method: 'GET',
       headers: {
@@ -39,7 +39,7 @@ class Api {
   }
 
   getInfoArray() {
-    return Promise.all([this._getUserInfo(), this._getCardslist()])
+    return Promise.all([this.getUser(), this._getCardslist()])
   }
 
   //Отправить запрос на отмену лайка
@@ -127,126 +127,3 @@ class Api {
 
 }
 
-// export {getCardslist, addCard, deleteCard, getUserInfo, patchUserProfile, getInfoArray, putLike, deleteLikeRequest, patchAvatar};
-
-// const token = '981a6ce5-6623-4e6e-80d4-27c651744329';
-// const serverURL = 'https://nomoreparties.co/v1/plus-cohort-1/'
-
-// //Обработка ответа сервера
-// const checkResponse = (res) => {
-//   if(res.ok) {
-//     return res.json();
-//   }
-//   else {
-//     return Promise.reject(`Ошибка ${res.status}`);
-//   }
-// }
-
-// // Запрос на отправку смены аватара
-// const patchAvatar = (avatarURL) => {
-//   return fetch(`${serverURL}users/me/avatar`, {
-//     method: 'PATCH',
-//     headers: {
-//       authorization: token,
-//       'Content-Type': 'application/json'
-//     },
-//     body: JSON.stringify({
-//       avatar: avatarURL
-//     })
-//   })
-//     .then(checkResponse)
-// }
-
-// //Отправить запрос на отмену лайка
-// const deleteLikeRequest = (cardId) => {
-//   return fetch(`${serverURL}cards/likes/${cardId}`, {
-//     method: 'DELETE',
-//     headers: {
-//       authorization: token
-//     }
-//   })
-//     .then(checkResponse)
-// }
-
-// //Отправить запрос на постановку лайка
-// const putLike = (cardId) => {
-//   return fetch(`${serverURL}cards/likes/${cardId}`, {
-//     method: 'PUT',
-//     headers: {
-//       authorization: token
-//     }
-//   })
-//     .then(checkResponse)
-// }
-
-// const getInfoArray = () => {
-//   // showPreloader(mainContainer);
-//   return Promise.all([getUserInfo(), getCardslist()])
-// }
-
-// //Получение информации о пользователе с сервера
-// const getUserInfo = () => {
-//   return fetch(`${serverURL}users/me`, {
-//     headers: {
-//       authorization: token
-//     }
-//   })
-//     .then(checkResponse)
-// }
-
-// //Получение массива карточек с сервера
-// const getCardslist = () => {
-//   return fetch(`${serverURL}cards`, {
-//     headers: {
-//       authorization: token
-//     }
-//   })
-//     .then(checkResponse)
-// }
-
-// //Редактирование профиля
-// const patchUserProfile = (newUserName, newUserActivity) => {
-//   return fetch(`${serverURL}users/me`, {
-//     method: 'PATCH',
-//     headers: {
-//       authorization: token,
-//       'Content-Type': 'application/json'
-//     },
-//     body: JSON.stringify({
-//       name: newUserName,
-//       about: newUserActivity
-//     })
-//   })
-//     .then(checkResponse)
-// }
-
-// //Добавление новой карточки
-// const addCard = (nameCard, linkCard) => {
-//   return fetch(`${serverURL}cards`, {
-//     method: 'POST',
-//     headers: {
-//       authorization: token,
-//       'Content-Type': 'application/json'
-//       },
-//     body: JSON.stringify({
-//       name: nameCard,
-//       link: linkCard
-//     })
-//   })
-//     .then(checkResponse)
-// }
-
-// //Удаление карточки
-// const deleteCard = (cardId) => {
-//   return fetch(`${serverURL}cards/${cardId}`, {
-//     method: 'DELETE',
-//     headers: {
-//       authorization: token
-//     }
-//   })
-//     .then((res) => {
-//       if(!res.ok) {
-//         return Promise.reject(`Ошибка ${res.status}`);
-//       }
-//     })
-// }
--- src/pages/index.js
+++ src/pages/index.js
@@ -88,12 +88,15 @@ const popupFormEditProfile = new PopupWithForm(userEditPopupTest, (newData) => {
 //Обновляем данные о юзере на сервере
 popupFormEditProfile.setEventListeners()
 
-//Открываем popup редактирования профиля
-buttonUserEdit.addEventListener('click', () => {
-  const userData = user.getUser();
-  editFormValidator.setInitialState();
+const setUserInput = function(userData) {
   nameForInput.value = userData.name;
   activityForInput.value = userData.about;
+}
+
+//Открываем popup редактирования профиля
+buttonUserEdit.addEventListener('click', () => {
+  editFormValidator.setInitialState();
+  user.getUserInfo(api.getUser, setUserInput)
   popupFormEditProfile.open();
 })
 
@@ -101,6 +104,13 @@ changeAvatar.addEventListener('click', ()=> {
   editAvatarValidator.setInitialState();
   popupEditAvatar.open();
 })
+
+//Кнопка открытия добавления карточки
+buttonAddCard.addEventListener('click', () =>{
+cardFormValidator.setInitialState();
+popupAddCard.open();
+});
+
 
 api.getInfoArray()                //Получаем стартовые данные с сервера
   .then(([userInfo, cards]) => {
@@ -129,78 +139,5 @@ api.getInfoArray()                //Получаем стартовые данн
     hidePreloader();
   })
 
-//Кнопка открытия добавления карточки
-buttonAddCard.addEventListener('click', () => popupAddCard.open());
-
-//editFormValidator.setInitialState(); //вызываем при открытии popup редактирования
-//cardFormValidator.setInitialState(); //вызываем при открытии popup добавления карточки
-//editAvatarValidator.setInitialState(); //вызываем при открытии popup редактирования аватарки
 
 
-//Это все ниже старый код потом для удаления-------------------------------------------
-//===============================================================
-
-// export {userEditorForm, addCardForm, cardsList, avatar, changeAvatarForm};
-// import {submitFormNewCard, largeImagePopup, loadCards, handleDeleteCard} from '../components/card.js';
-// import {handleOpenUserEditor, submitFormProfile, handleClosePopup, closePopup, openPopup, cardAddPopup, nameUser, activityUser, changeAvatarPopup,
-//         submitNewAvatar} from '../components/modal.js';
-// import {enableValidationForms} from '../components/validation.js';
-// import {setUserInfo} from '../components/user.js';
-// import {getInfoArray} from '../components/api.js';
-// import {hidePreloader, cardTarget} from '../components/util.js';
-// import './index.css';
-
-// const buttonUserEdit = document.querySelector('.profile__user-edit');        // Кнопка открытия редактирования профиля.
-// const buttonUserEditorClose = document.querySelector('#user-editor-close');  // Кнопка закрытия редактирования профиля.
-// const buttonAddCard = document.querySelector('.profile__add-card');          // Кнопка добавления карточки.
-// const buttonAddCardClose = document.querySelector('#add-card-close');        // Кнопка закрытия окна добавления изображения.
-// const buttonCloseViewImage = document.querySelector('#close-larger-image');  // Кнопка закрытия окна просмотра изображения.
-// const buttonChangeAvatar = document.querySelector('.profile__change-avatar');    // Кнопка открытия изменения аватара.
-// const userEditorForm = document.querySelector('#popup-user-edit-container'); // Форма редактирования профиля.
-// const addCardForm = document.querySelector('#popup-add-card-container');     // Форма добавления новой карточки.
-// const cardsList = document.querySelector('.cards__list');                    // Список содержащий карточки.
-// const avatar = document.querySelector('.profile__avatar');                   // Аватар
-// const changeAvatarForm = document.querySelector('#popup-change-avatar-container'); // Форма смены аватара.
-// const buttonCloseChangeAvatar = document.querySelector('#change-avatar-close'); //Кнопка закрытия окна изменения аватара.
-// const buttonCloseConfirmDelete = document.querySelector('#confirm-delete-close'); //Кнопка закрытия окна изменения аватара.
-// const buttonConfirmDelete = document.querySelector('#button-confirm-delete-card'); //Кнопка подтверждения удаления карточки.
-
-// // Popup User
-// buttonUserEdit.addEventListener('click', handleOpenUserEditor);
-// userEditorForm.addEventListener('submit', submitFormProfile);
-// buttonUserEditorClose.addEventListener('click', handleClosePopup);
-// // Popup добавления карточки
-// buttonAddCard.addEventListener('click', () => openPopup(cardAddPopup));
-// addCardForm.addEventListener('submit', submitFormNewCard);
-// buttonAddCardClose.addEventListener('click', handleClosePopup);
-// // Popup увеличенного изображения.
-// buttonCloseViewImage.addEventListener('click', () => closePopup(largeImagePopup));
-// // Popup обновления аватара
-// buttonChangeAvatar.addEventListener('click', () => openPopup(changeAvatarPopup));
-// buttonCloseChangeAvatar.addEventListener('click', handleClosePopup);
-// changeAvatarForm.addEventListener('submit', submitNewAvatar);
-// // Popup подтверждения удаления карточки
-// buttonCloseConfirmDelete.addEventListener('click', handleClosePopup);
-// buttonConfirmDelete.addEventListener('click', () => handleDeleteCard(cardTarget));
-
-// getInfoArray()
-//   .then(([userInfo, cards]) => {
-//     const userId = userInfo._id;
-//     loadCards(cardsList, cards, userId);
-//     setUserInfo(nameUser, activityUser, avatar, userInfo);
-//   })
-//   .catch((err) => {
-//     console.log(err);
-//   })
-//   .finally(() => {
-//     hidePreloader();
-//   })
-
-// enableValidationForms({
-//   formSelector: '.popup__main-container',
-//   inputSelector: '.popup__form',
-//   submitButtonSelector: '.popup__button-save',
-//   inactiveButtonClass: 'popup__button-save_inactive',
-//   inputErrorClass: 'popup__form_type_error',
-//   errorClass: 'popup__error_active'
-// });
